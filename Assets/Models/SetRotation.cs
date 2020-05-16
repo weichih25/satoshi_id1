c26907822d476522b4e01517123ffad248e4a606
weichih25 <weichih25@gmail.com>
2020-05-16 12:08:51 +0800

    project uploaded

---
+++ Assets/Models/SetRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float rotateAngle;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, this.rotateAngle * Time.deltaTime, 0);
+    }
+}
